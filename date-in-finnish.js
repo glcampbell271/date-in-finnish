677dc9e1c49d18265021e3a0c334cceee556f24e
George Campbell <glcampbell271@gmail.com>
2021-04-06 16:26:25 +0100

    Create date-in-finnish.js

---
+++ date-in-finnish.js
@@ -0,0 +1,7 @@
+/******************************************************************************\
+|*  date-in-finnish.js
+|*  Version: 0.0.1
+|*  Author: George Campbell
+|*  License: Apache License 2.0
+|*  Repository: https://github.com/glcampbell271/date-in-finnish
+\******************************************************************************/
