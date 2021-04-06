d30baacc3b55139245f7aca97968566864c70489
George Campbell <glcampbell271@gmail.com>
2021-04-06 17:36:49 +0100

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # date-in-finnish
 A little script to help you learn how to say a date in Finnish.
 
+Current version: 1.0.0
+
 **Disclaimer:** this is for very basic learning only.
 Colloquial speech is probably different.
--- date-in-finnish.js
+++ date-in-finnish.js
@@ -1,6 +1,6 @@
 /******************************************************************************\
 |*  date-in-finnish.js
-|*  Version: 0.0.1
+|*  Version: 1.0.0
 |*  Author: George Campbell
 |*  License: Apache License 2.0
 |*  Repository: https://github.com/glcampbell271/date-in-finnish
